3706fc16f8e9e1e7bb5155f5fcd5a0df0c2454e7
Senthil <senthilnathan.subash@colanonline.com>
2017-06-17 12:21:30 +0530

    Impl

--- src/main/java/com/friendsSuggestor/service/FriendSuggestorService.java
+++ src/main/java/com/friendsSuggestor/service/FriendSuggestorService.java
@@ -1,6 +1,9 @@
 package com.friendsSuggestor.service;
 
+import com.friendsSuggestor.model.FriendSuggestor;
+
 public interface FriendSuggestorService {
 
+	public FriendSuggestor save(FriendSuggestor friendSuggestor);
 	
 }
--- src/main/java/com/friendsSuggestor/service/impl/FriendSuggestorServiceImpl.java
+++ src/main/java/com/friendsSuggestor/service/impl/FriendSuggestorServiceImpl.java
@@ -3,6 +3,7 @@ package com.friendsSuggestor.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.friendsSuggestor.model.FriendSuggestor;
 import com.friendsSuggestor.repository.FriendSuggestorRepository;
 import com.friendsSuggestor.service.FriendSuggestorService;
 
@@ -13,7 +14,11 @@ public class FriendSuggestorServiceImpl implements FriendSuggestorService {
 
 	@Autowired	FriendSuggestorRepository friendSuggestorRepository;
 	
-	
+	public FriendSuggestor save(FriendSuggestor friendSuggestor){
+		FriendSuggestor frndSuggt = new FriendSuggestor();
+		frndSuggt = friendSuggestorRepository.save(friendSuggestor);
+		return frndSuggt;
+	}
 
 	
 }
